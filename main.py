94b1a764d6b04b801ccd229bf57e795d339da551
Promise Emeziem Adiole <promzy007adiole@yahoo.com>
2026-04-04 01:21:20 +0200

    function calling reasoning

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 def main():
     print("Hello from React LangGraph with Function Calling!")
-    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
